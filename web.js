00a78fdfcb53b63b3cc15fd43301608c46181806
Ubuntu <ubuntu@ip-172-31-44-66.us-west-2.compute.internal>
2013-07-23 20:43:34 +0000

    buggy version of bitstarter

--- web.js
+++ web.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = fs.readFileSync('index.html');
+  var index = fs.readFileSync(htmlfile);
   
   response.send(index.toString());
 });
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static('public/'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
